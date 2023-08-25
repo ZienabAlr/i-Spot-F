3527a3cc9f576453ae75e06c488a7936f881326d
ZienabAlr <r0784494@student.thomasmore.be>
2023-08-25 18:46:18 +0200

    feat(personalinfo.js): values of the inputs have been connected with objects in database

--- SignUp/personalinfo.js
+++ SignUp/personalinfo.js
@@ -2,12 +2,20 @@ export default class Info{
 
     postInfo(){
 
-        const name = document.querySelector(".name");
-        const birthdate = document.querySelector(".birthdate");
-        const gender = document.querySelector(".gender");
-        const study = document.querySelector(".study"); 
-        const email = document.querySelector(".email");
-    }
-       
+    const name = document.querySelector(".name");
+    const birthdate = document.querySelector(".birthdate");
+    const gender = document.querySelector(".gender");
+    const study = document.querySelector(".study"); 
+    const email = document.querySelector(".email");
+
+    const info = {
+        name: name.value,
+        birthdate: birthdate.value,
+        gender: gender.value,
+        study: study.value,
+        email: email.value
+    };
+
     
+}
 }
