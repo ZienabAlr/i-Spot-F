678dc18e2b683e37ef5f41a8622730eeaffc4ccc
ZienabAlr <r0784494@student.thomasmore.be>
2023-08-25 18:43:29 +0200

    feat(personalinfo.js): inputs with class names have been selected

--- SignUp/personalinfo.js
+++ SignUp/personalinfo.js
@@ -1,4 +1,13 @@
 export default class Info{
 
-  
+    postInfo(){
+
+        const name = document.querySelector(".name");
+        const birthdate = document.querySelector(".birthdate");
+        const gender = document.querySelector(".gender");
+        const study = document.querySelector(".study"); 
+        const email = document.querySelector(".email");
+    }
+       
+    
 }
