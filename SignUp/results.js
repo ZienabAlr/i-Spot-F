1aacb24a9d3f9652e41802870f60e34d50cac1be
ZienabAlr <r0784494@student.thomasmore.be>
2023-08-28 19:37:59 +0200

    fix(results.js): Average of each category has been calculated

--- SignUp/results.js
+++ SignUp/results.js
@@ -13,35 +13,30 @@ function getResults(){
         labels: ['Talen', 'Exacte Wetenschappen', 'Wiskunde', 'Literatuur', 'Medische Activiteiten', 'Sociale Wetenschappen', 'Handel-Zaken', 'Muziek', 'Sport', 'Plastisch Opvoeding', 'Techniek', 'Sociale Wetenschappen'],
         datasets: [{
             label: '# Results',
-            data: //giv the data of the last element in the array
-            [data.data.results[data.data.results.length - 1].talen,
-            data.data.results[data.data.results.length - 1].exacteWetenschappen,
-            data.data.results[data.data.results.length - 1].wiskunde,
-            data.data.results[data.data.results.length - 1].literatuur,
-            data.data.results[data.data.results.length - 1].medischeActiviteiten,
-            data.data.results[data.data.results.length - 1].socialeDienstbetoon,
-            data.data.results[data.data.results.length - 1].handelZaken,
-            data.data.results[data.data.results.length - 1].muziek,
-            data.data.results[data.data.results.length - 1].sport,
-            data.data.results[data.data.results.length - 1].plastischOpvoeding,
-            data.data.results[data.data.results.length - 1].techniek,
-            data.data.results[data.data.results.length - 1].socialeWetenschappen
+            data: //give the data of the last element in the array
+            [data.data.results[data.data.results.length - 1].talen / data.data.results.length - 1,
+            data.data.results[data.data.results.length - 1].exacteWetenschappen / data.data.results.length - 1,
+            data.data.results[data.data.results.length - 1].wiskunde / data.data.results.length - 1,
+            data.data.results[data.data.results.length - 1].literatuur / data.data.results.length - 1,
+            data.data.results[data.data.results.length - 1].medischeActiviteiten / data.data.results.length - 1,
+            data.data.results[data.data.results.length - 1].socialeDienstbetoon / data.data.results.length - 1,
+            data.data.results[data.data.results.length - 1].handelZaken / data.data.results.length - 1,
+            data.data.results[data.data.results.length - 1].muziek / data.data.results.length - 1,
+            data.data.results[data.data.results.length - 1].sport / data.data.results.length - 1,
+            data.data.results[data.data.results.length - 1].plastischOpvoeding / data.data.results.length - 1,
+            data.data.results[data.data.results.length - 1].techniek / data.data.results.length - 1,
+            data.data.results[data.data.results.length - 1].socialeWetenschappen / data.data.results.length - 1, 
+
+            console.log (data.data.results[data.data.results.length - 1].talen / data.data.results.length - 1)
+            // give the average of talen of the last element in the array
+
             ],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+        
+
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+         
             ],
             borderWidth: 1
         }]
@@ -57,17 +52,6 @@ function getResults(){
 
     }
     );
-    // console.log("results rray" ,results);
 
 }
 getResults();
-
-function showResults(){
-
-    // make a bar chart
-  
-    // console.log("results rray" ,results);
-    // console.log("results rray" ,results);
-
-
-}
